db0ce6ee419d9440ce0f561ec70312a426168257
unknown <amine.bd@gmail.com>
2022-03-05 14:29:41 +0100

    first commit Amine

---
+++ src/Components/Searchbar.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function Searchbar() {
+  return (
+    <div>
+    <input type="text" placeholder="Search ..."></input>
+    <button > Search </button>
+    </div>
+  )
+}
+
+export default Searchbar
